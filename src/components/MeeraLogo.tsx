f5cdab6424b3e6cca70b4f8a0bee736eeb485695
Vidhi Thakur <72219676+vidhi-thakur@users.noreply.github.com>
2024-12-17 21:43:45 +0530

    Updated header logo
    - added logo image in header

--- src/components/MeeraLogo.tsx
+++ src/components/MeeraLogo.tsx
@@ -1,16 +1,11 @@
+import Image from "next/image";
+import meeralogo from '../../assets/meeralogo.gif'
 import { Box } from "@mui/material";
 
 export default function MeeraLogo() {
-
-    // replace with logo image
     return (
-        <Box sx={{
-            width: "172px", height: "80px"
-        }} className="" >
-            <span>
-                MEERASIGHT
-            </span>
-            <span>- Expert eye care solutons -</span>
-        </Box >
+        <Box sx={{ display: "grid", placeItems: "center" }}>
+            <Image src={meeralogo} alt="MeeraSight Logo" style={{ width: "173px", height: "79px", objectFit: "contain", objectPosition: "center center" }} width="173" height="79" fetchPriority="low" loading="lazy" decoding="async" />
+        </Box>
     );
 }
