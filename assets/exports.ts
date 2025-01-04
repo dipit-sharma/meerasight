aa31fab2212219c6638f214fa6a38efa2aa581de
superstark02@gmail.com <superstark02@gmail.com>
2025-01-04 18:10:36 +0530

    DS | Images migrated

--- assets/exports.ts
+++ assets/exports.ts
@@ -1,69 +1,38 @@
-import welcome2 from "./welcome2.avif";
-import welcome3 from "./welcome3.avif";
-import welcome4 from "./welcome4.avif";
-import welcome5 from "./welcome5.avif";
-import welcome6 from "./welcome6.avif";
-import welcome7 from "./welcome7.avif";
-import vanshika from "./VanshikaYadav.avif";
-import jitendra from "./jitendra.avif";
-import nirmal from "./nirmal.avif";
-import trinabh from "./Trinabh.avif";
-import pradeep2 from "./pradeep2.avif";
-import vaibhavi from "./vaibhavi.avif";
-import sunil from "./SunilBhalla.avif";
-import rajkumarsharma from "./RajKumarSharma.avif";
-import rajkumar from "./RajKumar1.avif";
-import cataract from "./cataract.avif";
-import comprehensive_eye_examination from "./comprehensive_eye_examination.avif";
-import contact_lens_clinic from "./contact_lens_clinic.avif";
-import cornea_services from "./cornea_services.avif";
-import glaucoma_services from "./glaucoma_services.avif";
-import keratoconous from "./keratoconous.avif";
-import event1 from "./event1.avif";
-import event2 from "./event2.avif";
-import event3 from "./event3.avif";
-import event4 from "./event4.avif";
-import event5 from "./event5.avif";
-import event6 from "./event6.avif";
-import event7 from "./event7.avif";
-import event8 from "./event8.avif";
-
 const welcome1 = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fwelcome1.avif?alt=media&token=7b865001-94d1-4066-b51e-2063323f3075`;
 const retina_services = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fretina_services.avif?alt=media&token=326f04ef-a5b5-4e41-8e9c-c5313e5b3fa7`;
 const refractive_services = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Frefractive_services.avif?alt=media&token=10504831-6cf2-4986-b1cc-8afacac35988`;
 const pediatric_eye_diseases = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fpediatric_eye_diseases.avif?alt=media&token=f509d6ae-22de-4e36-a3c0-3183b99d806e`;
 const ocular_surface_diseases_management = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Focular_surface_diseases_management.avif?alt=media&token=80164ac5-7921-4996-a9b0-1dac7176db49`;
-// keratoconous,
-// glaucoma_services,
-// cornea_services,
-// contact_lens_clinic,
-// comprehensive_eye_examination,
-// cataract,
-// vaibhavi,
-// rajkumar,
-// rajkumarsharma,
-// sunil,
-// pradeep2,
-// trinabh,
-// nirmal,
-// jitendra,
-// vanshika,
-// welcome1,
-// welcome2,
-// welcome3,
-// welcome4,
-// welcome5,
-// welcome6,
-// welcome7,
+const keratoconous = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fkeratoconous.avif?alt=media&token=73d07546-a976-4d97-a703-7bbadaebaf07`;
+const glaucoma_services = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fglaucoma_services.avif?alt=media&token=c0d77f75-4675-47d1-b311-c65f6924f3a2`;
+const cornea_services = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fcornea_services.avif?alt=media&token=bd8d0bf5-8d1d-493e-a782-77afc148e2f2`;
+const contact_lens_clinic = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fcontact_lens_clinic.avif?alt=media&token=36a4a903-b65f-4204-8c59-c9317e320b86`;
+const comprehensive_eye_examination = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fcomprehensive_eye_examination.avif?alt=media&token=3ba525a1-6524-47ec-8dda-ad7e57d8dc5f`;
+const cataract = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fcataract.avif?alt=media&token=11778eae-4894-40fa-ac5a-13c0c0212959`;
+const vaibhavi = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fvaibhavi.avif?alt=media&token=7c082b6d-2815-4ea2-b960-dae99e3279d9`;
+const rajkumar = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2FRajKumar1.avif?alt=media&token=99a65527-6466-4932-b0bb-74c61ebd943f`;
+const rajkumarsharma = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2FRajKumarSharma.avif?alt=media&token=7f2cb0d3-1561-4689-8b3f-d377ce33a6b6`;
+const sunil = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2FSunilBhalla.avif?alt=media&token=02eac446-16d3-4739-9645-7773de7c8cff`;
+const pradeep2 = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fpradeep2.avif?alt=media&token=8da1e5a1-50c5-4849-881e-1ada89f90ebf`;
+const trinabh = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2FTrinabh.avif?alt=media&token=24f360b9-601b-4afe-87fe-2228a3b082a6`;
+const nirmal = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fnirmal.avif?alt=media&token=30251e5b-3c5f-44e9-9d40-37e377441a6d`;
+const jitendra = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fjitendra.avif?alt=media&token=06bfee81-fc36-4da0-bb1a-ffe049ee0a96`;
+const vanshika = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2FVanshikaYadav.avif?alt=media&token=7fdf3b50-32ef-494a-b4b3-4032a4909806`;
+const welcome2 = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fwelcome2.avif?alt=media&token=096002f8-5a3b-47f8-b084-a04a2249a02e`;
+const welcome3 = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fwelcome3.avif?alt=media&token=b42733a5-badd-4719-8fa3-fda35dd8f633`;
+const welcome4 = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fwelcome4.avif?alt=media&token=1c776369-c36f-45c2-ad07-7d32c0b1a1e2`;
+const welcome5 = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fwelcome5.avif?alt=media&token=9f59c681-182a-44c3-8b66-a074aeb29c93`;
+const welcome6 = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fwelcome6.avif?alt=media&token=069d4222-ace7-4009-9df2-b34bad05ecc7`;
+const welcome7 = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fwelcome7.avif?alt=media&token=c0dd3efc-58d4-4f07-bb54-2313acfb50da`;
 const meeralogo = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fmeeralogo.gif?alt=media&token=fa520722-1118-40c4-b2fc-dc8e8118dc96`;
-// event1,
-// event2,
-// event3,
-// event4,
-// event5,
-// event6,
-// event7,
-// event8,
+const event1 = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fevent1.avif?alt=media&token=650472cf-6315-4e02-9b2d-70d865d226cc`;
+const event2 = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fevent2.avif?alt=media&token=e41f55c7-f93d-45d9-8ae5-7544a261871c`;
+const event3 = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fevent3.avif?alt=media&token=da8cc4ce-23c1-47c7-af28-595a2bdd846a`;
+const event4 = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fevent4.avif?alt=media&token=7b4fc493-9a7a-4995-86c5-8a4bbadca30f`;
+const event5 = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fevent5.avif?alt=media&token=0c2c871f-60f8-42cc-ad73-1b728263ddcf`;
+const event6 = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fevent6.avif?alt=media&token=c43d1bbf-bd84-411a-aba5-2a890ad68b55`;
+const event7 = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fevent6.avif?alt=media&token=c43d1bbf-bd84-411a-aba5-2a890ad68b55`;
+const event8 = `https://firebasestorage.googleapis.com/v0/b/meerasight-acf91.firebasestorage.app/o/images%2Fevent8.avif?alt=media&token=e46fced1-8da4-4a97-9f61-95bb6cf08b47`;
 
 export {
   retina_services,
